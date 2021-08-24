e223cf7209734dbaff8c9dcda47f67e5a95e8247
zhaochong <1947606046@qq.com>
2021-08-25 00:05:46 +0800

    23:45

---
+++ src/guanjianzi/String_.java
@@ -0,0 +1,15 @@
+package guanjianzi;
+
+public class String_ {
+    public static void main(String[] args) {
+//        String s = "hello2";
+//        final String s2 = "hello";
+//        String s3 = s2 + 2;
+//        String s4  = s2 +2;
+//        System.out.println(s3 == s4);
+//        System.out.println(s == s3);
+//        System.out.println(s == s2);
+//        System.out.println(s2 == s3);
+    }
+
+}
